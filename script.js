b1fc3309c514b68510b8f365d5123714ed071621
emwolfslu <78510551+emwolfslu@users.noreply.github.com>
2021-02-15 13:10:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
+require([
+      "esri/Map",
+      "esri/layers/CSVLayer",
+      "esri/views/MapView",
+      "esri/config",
+      "esri/core/urlUtils",
+      "dojo/domReady!"
+    ], function(
+      Map,
+      CSVLayer,
+      MapView,
+      esriConfig,
+      urlUtils
+    ) {
+
+      // If CSV files are not on the same domain as your website, a CORS enabled server
+      // or a proxy is required.
+     var url = "https://raw.githubusercontent.com/gbrunner/Advanced_Python_for_GIS_and_RS/master/Week%202/stl_crime_wgs_84.csv";
+     esriConfig.request.corsEnabledServers.push('https://rawgit.com');
+
+      
+
+        const template = {
+          title: "Saint Louis Crime",
+          
+        };
+
+        const csvLayer = new CSVLayer({
+          url: url,
+          copyright: "St. Louis Police Department",
+          popupTemplate: template
         });
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 10,
-          center: [-89.85, 38.85] // longitude, latitude
-        });
+        var symbol = {
+          type: "simple-marker", 
+          color:"green"
+        };
+
+      csvLayer.renderer = {
+        type: "simple", // autocasts as new SimpleRenderer()
+        symbol: symbol
+      };
+
+      var map = new Map({
+        basemap: "gray",
+        layers: [csvLayer]
       });
+
+      var view = new MapView({
+        container: "viewDiv",
+        center: [-90.199402, 38.627003],
+        zoom: 11,
+        map: map
+      });
+
+    });
